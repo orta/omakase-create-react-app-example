f4f605812e767bd144030737d0114e21fb422f8b
Orta Therox <orta.therox@gmail.com>
2018-10-30 11:49:33 -0400

    SC + TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components/macro'
+
+const RedP = styled.p`
+  color: red;
+`
 
 class App extends Component {
   render() {
@@ -8,9 +13,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
+
+          <RedP>
             Edit <code>src/App.js</code> and save to reload.
-          </p>
+          </RedP>
           <a
             className="App-link"
             href="https://reactjs.org"
